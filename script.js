99c28e3bba1e80887a79f01deb38c3bacace5ea3
medox <medox@MacBook-Air-de-Crypto-999.local>
2020-10-18 20:49:11 +0100

    airplane porject

---
+++ script.js
@@ -0,0 +1,16 @@
+var f1  = document.getElementById("f1");
+var f2  = document.querySelector("#f2");
+var f3 = document.querySelector("#fs");
+// window.addEventListener("load", function(){
+//     document.getElementsByTagName("article")[0].style.display="block";
+//     document.getElementsByTagName("section")[0].style.display="none";
+// });
+
+
+
+
+
+
+
+
+
